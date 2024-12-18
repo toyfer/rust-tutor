a8b500bf684f54b555ebe0c6970524b46ec8f371
t.irokawa.public <t.irokawa.public@gmail.com>
2024-12-18 13:50:48 +0000

    function and method

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,8 @@
 fn main() {
-    let x = 5;
+    let result = add(3, 4); // 関数を呼び出す
+    println!("result: {}", result); //出力：result: 7
+}
 
-    if x > 5 {
-        println!("x is greater than 5");
-    } else if x == 5 {
-        println!("x is equal to 5");
-    } else {
-        println!("x is less than 5");
-    }
-
-    let mut count = 0;
-    loop {
-        count += 1;
-        if count == 3 {
-            break;
-        }
-    }
-    println!("count: {}", count); //出力：count: 3
-
-
-    let mut i = 0;
-    while i < 5 {
-        println!("{}", i);
-        i += 1;
-    }
-
-    for i in 0..5 { // 0から4まで
-        println!("{}", i);
-    }
+fn add(x: i32, y: i32) -> i32 { // 関数を定義する
+    x + y
 }
