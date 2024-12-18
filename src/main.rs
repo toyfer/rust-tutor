4ce10eb8908b5a5e45a87ea0a07b2a5d4299f1de
t.irokawa.public <t.irokawa.public@gmail.com>
2024-12-18 13:50:12 +0000

    if else loop while for ok

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
-rust
 fn main() {
-    let x = 5; // xは不変
-    // x = 6; // エラー：不変の変数を変更しようとしました
+    let x = 5;
 
-    let mut y = 5; // yは可変
-    y = 6; // OK
-    println!("{}", y); // 出力: 6
+    if x > 5 {
+        println!("x is greater than 5");
+    } else if x == 5 {
+        println!("x is equal to 5");
+    } else {
+        println!("x is less than 5");
+    }
+
+    let mut count = 0;
+    loop {
+        count += 1;
+        if count == 3 {
+            break;
+        }
+    }
+    println!("count: {}", count); //出力：count: 3
+
+
+    let mut i = 0;
+    while i < 5 {
+        println!("{}", i);
+        i += 1;
+    }
+
+    for i in 0..5 { // 0から4まで
+        println!("{}", i);
+    }
 }
