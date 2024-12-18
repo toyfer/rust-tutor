ddd1b42e4ce1c5ee2f3d93d63213c4542be73a60
t.irokawa.public <t.irokawa.public@gmail.com>
2024-12-18 13:49:34 +0000

    Variables and mutability

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 rust
 fn main() {
-    println!("Hello, world!");
+    let x = 5; // xは不変
+    // x = 6; // エラー：不変の変数を変更しようとしました
+
+    let mut y = 5; // yは可変
+    y = 6; // OK
+    println!("{}", y); // 出力: 6
 }
