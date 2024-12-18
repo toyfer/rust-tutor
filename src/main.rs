547acf6db88d038a646a58bd9759a648ade929bd
t.irokawa.public <t.irokawa.public@gmail.com>
2024-12-18 13:57:48 +0000

    tuple

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
-    let s = "hello"; // 文字列リテラル
-    println!("{}", s); // 出力: hello
+    // それぞれの要素に対して、異なる型の定義が可能
+    let tup: (i32, f64 ,u8) = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+    println!("The value of y is: {}", y);
+
+    let x: i32 = tup.0;
+    let y: f64 = tup.1;
+    let z: u8 = tup.2;
+    println!("x is {}, y is {}, and z is {}", x,y,z);
 }
